11a3b4db3763ab199c56b7a9a910cd81a54ce403
kpchamp <kpchamp@uw.edu>
2019-09-18 11:51:26 -0700

    cleaning up/adding commenting to Lorenz example code

--- example_lorenz.py
+++ example_lorenz.py
@@ -1,18 +1,27 @@
 import numpy as np
 from scipy.integrate import odeint
-from scipy.special import legendre
+from scipy.special import legendre, chebyt
 from sindy_utils import library_size
 
 
-def get_lorenz_data(n_ics):
+def get_lorenz_data(n_ics, noise_strength=0):
+    """
+    Generate a set of Lorenz training data for multiple random initial conditions.
+
+    Arguments:
+        n_ics - Integer specifying the number of initial conditions to use.
+        noise_strength - Amount of noise to add to the data.
+
+    Return:
+        data - Dictionary containing elements of the dataset. See generate_lorenz_data()
+        doc string for list of contents.
+    """
     t = np.arange(0, 5, .02)
     n_steps = t.size
     N = 128
     
     ic_means = np.array([0,0,25])
     ic_widths = 2*np.array([36,48,41])
-
-    noise_strength = 1e-6
 
     # training data
     ics = ic_widths*(np.random.rand(n_ics, 3)-.5) + ic_means
@@ -24,10 +33,15 @@ def get_lorenz_data(n_ics):
     return data
 
 
-def lorenz_coefficients(normalization, poly_order=3):
-    sigma = 10
-    beta = 8/3
-    rho = 28
+def lorenz_coefficients(normalization, poly_order=3, sigma=10., beta=8/3, rho=28.):
+    """
+    Generate the SINDy coefficient matrix for the Lorenz system.
+
+    Arguments:
+        normalization - 3-element list of array specifying scaling of each Lorenz variable
+        poly_order - Polynomial order of the SINDy model.
+        sigma, beta, rho - Parameters of the Lorenz system
+    """
     Xi = np.zeros((library_size(3,poly_order),3))
     Xi[1,0] = -sigma
     Xi[2,0] = sigma*normalization[0]/normalization[1]
@@ -39,10 +53,18 @@ def lorenz_coefficients(normalization, poly_order=3):
     return Xi
 
 
-def simulate_lorenz(z0, t):
-    sigma = 10.
-    beta = 8/3
-    rho = 28 
+def simulate_lorenz(z0, t, sigma=10., beta=8/3, rho=28.):
+    """
+    Simulate the Lorenz dynamics.
+
+    Arguments:
+        z0 - Initial condition in the form of a 3-value list or array.
+        t - Array of time points at which to simulate.
+        sigma, beta, rho - Lorenz parameters
+
+    Returns:
+        z, dz, ddz - Arrays of the trajectory values and their 1st and 2nd derivatives.
+    """
     f = lambda z,t : [sigma*(z[1] - z[0]), z[0]*(rho - z[2]) - z[1], z[0]*z[1] - beta*z[2]]
     df = lambda z,dz,t : [sigma*(dz[1] - dz[0]),
                           dz[0]*(rho - z[2]) + z[0]*(-dz[2]) - dz[1],
@@ -59,11 +81,27 @@ def simulate_lorenz(z0, t):
     return z, dz, ddz
 
 
-def generate_lorenz_data(ics, t, n_points, linear=True, normalization=None):
-    sigma = 10.
-    beta = 8/3
-    rho = 28 
-    f = lambda z,t : [sigma*(z[1] - z[0]), z[0]*(rho - z[2]) - z[1], z[0]*z[1] - beta*z[2]]
+def generate_lorenz_data(ics, t, n_points, linear=True, normalization=None,
+                            sigma=10, beta=8/3, rho=28):
+    """
+    Generate high-dimensional Lorenz data set.
+
+    Arguments:
+        ics - Nx3 array of N initial conditions
+        t - array of time points over which to simulate
+        n_points - size of the high-dimensional dataset created
+        linear - Boolean value. If True, high-dimensional dataset is a linear combination
+        of the Lorenz dynamics. If False, the dataset also includes cubic modes.
+        normalization - Optional 3-value array for rescaling the 3 Lorenz variables.
+        sigma, beta, rho - Parameters of the Lorenz dynamics.
+
+    Returns:
+        data - Dictionary containing elements of the dataset. This includes the time points (t),
+        spatial mapping (y_spatial), high-dimensional modes used to generate the full dataset
+        (modes), low-dimensional Lorenz dynamics (z, along with 1st and 2nd derivatives dz and
+        ddz), high-dimensional dataset (x, along with 1st and 2nd derivatives dx and ddx), and
+        the true Lorenz coefficient matrix for SINDy (Xi).
+    """
 
     n_ics = ics.shape[0]
     n_steps = t.size
@@ -74,7 +112,7 @@ def generate_lorenz_data(ics, t, n_points, linear=True, normalization=None):
     dz = np.zeros(z.shape)
     ddz = np.zeros(z.shape)
     for i in range(n_ics):
-        z[i], dz[i], ddz[i] = simulate_lorenz(ics[i], t)
+        z[i], dz[i], ddz[i] = simulate_lorenz(ics[i], t, sigma=sigma, beta=beta, rho=rho)
 
 
     if normalization is not None:
@@ -89,6 +127,8 @@ def generate_lorenz_data(ics, t, n_points, linear=True, normalization=None):
     modes = np.zeros((2*d, n))
     for i in range(2*d):
         modes[i] = legendre(i)(y_spatial)
+        # modes[i] = chebyt(i)(y_spatial)
+        # modes[i] = np.cos((i+1)*np.pi*y_spatial/2)
     x1 = np.zeros((n_ics,n_steps,n))
     x2 = np.zeros((n_ics,n_steps,n))
     x3 = np.zeros((n_ics,n_steps,n))
@@ -123,13 +163,14 @@ def generate_lorenz_data(ics, t, n_points, linear=True, normalization=None):
                           + modes[5]*(6*z[i,j,2]*dz[i,j,2]**2 + 3*(z[i,j,2]**2)*ddz[i,j,2])
 
     if normalization is None:
-        Xi = lorenz_coefficients([1,1,1])
+        Xi = lorenz_coefficients([1,1,1], sigma=sigma, beta=beta, rho=rho)
     else:
-        Xi = lorenz_coefficients(normalization)
+        Xi = lorenz_coefficients(normalization, sigma=sigma, beta=beta, rho=rho)
 
     data = {}
     data['t'] = t
     data['y_spatial'] = y_spatial
+    data['modes'] = modes
     data['x'] = x
     data['dx'] = dx
     data['ddx'] = ddx
